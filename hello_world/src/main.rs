ce69e6619450d89774cb492bb03dc878e8589644
Szymon Stosik <u047@losto.net>
2021-10-25 18:08:13 +0200

    Added hello_world Rust cargo

---
+++ hello_world/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![world])
+        .launch()
+        .await;
+}
