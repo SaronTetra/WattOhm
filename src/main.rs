ccafc0bf891268df36980eb5e5bc17cb54b99619
Szymon Stosik <u047@losto.net>
2021-10-29 00:40:23 +0200

    Add diesel, Add test from first part of tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #[macro_use] extern crate rocket;
 
+// extern crate diesel
+
+
+
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
+
+#[get("/<id>")]
+fn db(id: i32) -> String {
+    format!("id:{0}", id)
+}
+
+
 
 
 #[rocket::main]
 async fn main() {
     rocket::build()
         .mount("/", routes![world])
+        .mount("/db", routes![db])
         .launch()
         .await;
 }
