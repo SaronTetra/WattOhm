8f49ec277ef7473a617e42b98760d280420513b6
Maciej Stosik <starwars.maciek@gmail.com>
2021-10-28 17:56:00 +0200

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
-
 #[rocket::main]
 async fn main() {
-    rocket::build()
-        .mount("/", routes![world])
-        .launch()
-        .await;
+    rocket::build().mount("/", routes![world]).launch().await;
 }
