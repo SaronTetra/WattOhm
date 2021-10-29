335a8bf410128fd03720034b529702cb9ef7d750
Maciej S <starwars.maciek@gmail.com>
2021-10-29 10:25:58 +0200

    Fix jenkins pipeline (#6)
    * Add docker agent
    
    * Revert "Add docker agent"
    
    This reverts commit 3b17aa998149544309242fc7c02e04e5a5ef7715.
    
    * Show version
    
    * Add build stage
    
    * Change stages order
    
    * Added Jenkinsfile
    
    * Fix formatting
    
    * Update Jenkinsfile
    
    * Fix stash target
    
    * Added Jenkinsfile
    
    * Added Jenkinsfile
    
    * Allow unused
    
    * Added Jenkinsfile
    
    * Added Jenkinsfile
    
    * Added Jenkinsfile
    
    * Remove audit
    
    Will add in a seperate job later if necessary

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
-
+#[allow(unused_must_use)]
 #[rocket::main]
 async fn main() {
-    rocket::build()
-        .mount("/", routes![world])
-        .launch()
-        .await;
+    rocket::build().mount("/", routes![world]).launch().await;
 }
