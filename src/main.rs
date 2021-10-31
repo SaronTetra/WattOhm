315d1fe5a34e653baf779dfbfe48d9a14c7b8ec0
Szymon Stosik <58011139+SzymCrusader@users.noreply.github.com>
2021-10-31 16:56:54 +0100

    Add/5/postgresql (#11)
    * Add docker-compose PostgreSQL
    
    * Delete postgres/sql directory
    
    * Fix Postgres
    
    * Fix Postgres
    
    * Add diesel, Add test from first part of tutorial
    
    * Changes by Sarian
    
    * Move docker-compose.yml to main folder

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
+// extern crate diesel
+
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<id>")]
+fn db(id: i32) -> String {
+    format!("id:{0}", id)
+}
+
 #[allow(unused_must_use)]
 #[rocket::main]
 async fn main() {
-    rocket::build().mount("/", routes![world]).launch().await;
+    rocket::build()
+        .mount("/", routes![world])
+        .mount("/db", routes![db])
+        .launch()
+        .await;
 }
