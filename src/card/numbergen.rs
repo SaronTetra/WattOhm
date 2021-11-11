b3703b71710cbab84528882ef3c7e2ed0f75763d
Szymon Stosik <58011139+SzymCrusader@users.noreply.github.com>
2021-11-11 01:31:56 +0100

    Add/16/card generation (#17)
    * Add number generation
    
    * Add Luhn algorithm
    
    * Fix code formatting
    
    * Fix code formatting
    
    * Fix code formatting
    
    * Fix clippy

---
+++ src/card/numbergen.rs
@@ -0,0 +1,49 @@
+use crate::card::inttovec::number_to_vec;
+use rand::Rng;
+
+pub fn generate() -> String {
+    let mut rng = rand::thread_rng();
+    let mut card: String = String::new();
+    let mut number: Vec<u8> = vec![5, 3, 2, 3, 1, 5]; //532315 - WattOhm Issuer Identification Number (IIN)
+
+    // fill number with random digits
+    let mut i: u8 = 0;
+    let mut x: u8;
+    while i < 9 {
+        x = rng.gen_range(0..10);
+        number.push(x);
+        i += 1;
+    }
+
+    //Luhn
+    i = 2;
+    x = 0;
+    let mut y: u8;
+    number.reverse();
+    for &digit in &number {
+        y = digit * i;
+
+        if y >= 10 {
+            for s in number_to_vec(y) {
+                x += s;
+            }
+        } else {
+            x += y;
+        }
+
+        if i == 2 {
+            i = 1;
+        } else if i == 1 {
+            i = 2;
+        }
+    }
+    y = 10 - (x % 10);
+    number.reverse();
+    number.push(y);
+
+    // transform number:Vec to String
+    for digit in number {
+        card += &digit.to_string();
+    }
+    card
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
+mod card;
+
 // extern crate diesel
 
 #[get("/")]
@@ -13,12 +15,19 @@ fn db(id: i32) -> String {
     format!("id:{0}", id)
 }
 
+#[get("/")]
+fn cardgen() -> String {
+    let number: String = card::numbergen::generate();
+    format!("Card number: {0}", number)
+}
+
 #[allow(unused_must_use)]
 #[rocket::main]
 async fn main() {
     rocket::build()
         .mount("/", routes![world])
         .mount("/db", routes![db])
+        .mount("/card", routes![cardgen])
         .launch()
         .await;
 }
